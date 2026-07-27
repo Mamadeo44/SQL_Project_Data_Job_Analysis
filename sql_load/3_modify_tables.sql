b0f2e48f5c42e3f9ed37a0c593ba0ec2e1dc0504
Mamadeo44 <mamadoudieye44@gmail.com>
2026-07-27 16:44:12 +0200

    Initial commit

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,105 @@
+/* ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
+Database Load Issues (follow if receiving permission denied when running SQL code below)
+
+Possible Errors: 
+- ERROR >>  duplicate key value violates unique constraint "company_dim_pkey"
+- ERROR >> could not open file "C:\Users\...\company_dim.csv" for reading: Permission denied
+
+1. Drop the Database 
+            DROP DATABASE IF EXISTS sql_course;
+2. Repeat steps to create database and load table schemas
+            - 1_create_database.sql
+            - 2_create_tables.sql
+3. Open pgAdmin
+4. In Object Explorer (left-hand pane), navigate to `sql_course` database
+5. Right-click `sql_course` and select `PSQL Tool`
+            - This opens a terminal window to write the following code
+6. Get the absolute file path of your csv files
+            1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
+7. Paste the following into `PSQL Tool`, (with the CORRECT file path)
+
+\copy company_dim FROM '/Users/mamadoudieye/Documents/dev/SQL_Project_Data_Job_Analysis/csv_files/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\copy skills_dim FROM '/Users/mamadoudieye/Documents/dev/SQL_Project_Data_Job_Analysis/csv_files/skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\copy job_postings_fact FROM '/Users/mamadoudieye/Documents/dev/SQL_Project_Data_Job_Analysis/csv_files/job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\copy skills_job_dim FROM '/Users/mamadoudieye/Documents/dev/SQL_Project_Data_Job_Analysis/csv_files/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+*/
+
+-- NOTE: This has been updated from the video to fix issues with encoding
+COPY company_dim
+FROM '/Users/mamadoudieye/Documents/dev/SQL_Project_Data_Job_Analysis/csv_files/company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_dim
+FROM '/Users/mamadoudieye/Documents/dev/SQL_Project_Data_Job_Analysis/csv_files/skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY job_postings_fact
+FROM '/Users/mamadoudieye/Documents/dev/SQL_Project_Data_Job_Analysis/csv_files/job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_job_dim
+FROM '/Users/mamadoudieye/Documents/dev/SQL_Project_Data_Job_Analysis/csv_files/skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+
+SELECT *
+FROM company_dim
+LIMIT 100;
+
+
+CREATE TABLE january_jobs AS 
+   SELECT *
+   FROM job_postings_fact
+   WHERE EXTRACT(MONTH FROM job_posted_date)=1;
+
+
+CREATE TABLE february_jobs AS 
+   SELECT *
+   FROM job_postings_fact
+   WHERE EXTRACT(MONTH FROM job_posted_date)=2;
+
+
+CREATE TABLE march_jobs AS 
+   SELECT *
+   FROM job_postings_fact
+   WHERE EXTRACT(MONTH FROM job_posted_date)=3;
+
+SELECT job_posted_date
+FROM march_jobs;
+
+
+
+SELECT *
+FROM (-- SubQuery starts here
+    SELECT * 
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=1
+) AS january_jobs;
+-- SubQuery ends here 
+
+WITH january_jobs AS ( -- CTE definition starts here 
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date)=1
+) -- CTE definition ends here 
+
+SELECT * FROM january_jobs;
+
+
+DROP TABLE IF EXISTS january_jobs;
+DROP TABLE IF EXISTS february_jobs;
+DROP TABLE IF EXISTS march_jobs;
+
+
+
+
+
+
+
+
+
+
